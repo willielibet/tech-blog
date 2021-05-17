253d9fdf036bf3e0881219c68ca16e5ed6353ef2
Willie <write2willie@gmail.com>
2021-05-17 15:55:47 -0400

    commit #3: created config.js file for db connection, may172021, 3.55pm

---
+++ config/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+// create connection to our db
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
+
+const sequelize = new Sequelize(process.env.MYSQL_URL)
+module.exports = sequelize;
