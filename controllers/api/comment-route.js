f322dd9111daf73c3e0ebac1bfc3c3fec3a4b19f
Willie <write2willie@gmail.com>
2021-05-20 09:23:51 -0400

    commit #22: added route to create comments, may202021, 9.23am

---
+++ controllers/api/comment-route.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { Comment } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+//activity 28 controllers/homeRoutes.js.
+//activity 28 controllers/projectRouts.js
+//use withAuth middleware to prevent access to route.
+//if you are logged in, then you can create new blog posts.
+//this way the / route and all its child routes will be protected 
+//by our authentication middleware.
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
+    res.json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
