7728c38f771da3a2d4c0576dd5bfbacf0fe6f951
Willie <write2willie@gmail.com>
2021-05-19 20:32:08 -0400

    commit #19: added routes to match login user and logout, may192021, 8.32pm

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,26 +1,77 @@
-    const router = require('express').Router();
-    const { User } = require('../../models');
+const router = require('express').Router();
+const { User } = require('../../models');
 
-    //activities 23 and 24
-    //create a new user.
-    router.post('/', async (req, res) => {
+//activity 23 controllers/api/userRoutes.js 
+//acitivy 24
+//create a new user.
+router.post('/', async (req, res) => {
+try {
+    const newUser = await User.create({
+    //assign the entered username and password to 
+    //the username and password.
+    username: req.body.username,
+    password: req.body.password,
+    });
+
+    // create session variables based on the logged in user
+    req.session.save(() => {
+    req.session.userId = newUser.id;
+    req.session.username = newUser.username;
+    req.session.loggedIn = true;
+
+    res.json(newUser);
+    });
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+
+router.post('/login', async (req, res) => {
     try {
-        const newUser = await User.create({
-        //assign the entered username and password to 
-        //the username and password.
+    // find the user who matches the posted username address
+    const user = await User.findOne({
+        where: {
         username: req.body.username,
-        password: req.body.password,
-        });
+        },
+    });
 
-        // create session variables based on the logged in user
-        req.session.save(() => {
-        req.session.userId = newUser.id;
-        req.session.username = newUser.username;
+    if (!user) {
+        res.status(400).json({ message: 'No user found!' });
+        return;
+    }
+
+    // verify the posted password with the password store in the database.
+    const validPassword = user.checkPassword(req.body.password);
+    // if password is not valid
+    if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect user or password, please try again!' });
+        return;
+    }
+
+    // create session variables based on the logged in user
+    req.session.save(() => {
+        req.session.userId = user.id;
+        req.session.username = user.username;
         req.session.loggedIn = true;
 
-        res.json(newUser);
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+        res.json({ user, message: 'You are now logged in!' });
     });
+    } catch (err) {
+    res.status(400).json({ message: 'No user found!' });
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      //remove the session variables
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+  module.exports = router;
+  
